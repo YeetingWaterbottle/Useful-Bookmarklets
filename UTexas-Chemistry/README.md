fcb2810b33690fb9b5912ddd69ad85cde9cbb251
YeetingWaterbottle <73762047+YeetingWaterbottle@users.noreply.github.com>
2023-03-02 22:11:27 -0800

    fix and update code for UTexas Chemistry

--- UTexas-Chemistry/README.md
+++ UTexas-Chemistry/README.md
@@ -1,2 +1,9 @@
-# This script will enable scrolling for the answer box on the right
+# This script will enable scrolling for the answer box on the right (Updated 2/27/2023)
+
+## Other issues fixed:
+
+-   **Prevent site from jumping to the first question after each submission**
+-   **Stops the question from dissappearing after window change**
+-   **Removed giant gap of white space below the questions**
+
 ![Script displays additional information](enable-answer-box-scrolling.png)
--- UTexas-Chemistry/enable-answer-box-scrolling.js
+++ UTexas-Chemistry/enable-answer-box-scrolling.js
@@ -1,11 +1,9 @@
 // Bookmarklet Created With Bookmarkleter: https://chriszarate.github.io/bookmarkleter/
 // Copy the line below and create a bookmark with the url field set to it ⤵
-javascript:void function(){let a=document.querySelector(".col-md-4");a.style.maxHeight="1200px",a.style.overflow="auto"}();
-
+javascript:void function(){document.querySelector(".dataTables_scrollBody").style="position: unset !important; overflow: unset !important; height: unset !important;",document.querySelector(".dataTables_scrollBody > .table").style="position: unset !important;",document.querySelector(".dataTables_scrollBody > div").style="height: inherit !important;",document.querySelector(".col-sm-12").style="max-height: 1200px !important; overflow: auto !important;"}();
 /* Original Javascript Code Below
-let answer_box = document.querySelector(".col-md-4")
-
-answer_box.style.maxHeight = "1200px"
-answer_box.style.overflow = "auto"
-
+document.querySelector('.dataTables_scrollBody').style = 'position: unset !important; overflow: unset !important; height: unset !important;';
+document.querySelector('.dataTables_scrollBody > .table').style = 'position: unset !important;';
+document.querySelector('.dataTables_scrollBody > div').style = 'height: inherit !important;';
+document.querySelector('.col-sm-12').style = 'max-height: 1200px !important; overflow: auto !important;';
 */
