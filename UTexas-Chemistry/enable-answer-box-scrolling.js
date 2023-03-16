7f9a73d19e8dd175f4dafb28c554ad1cf60bc6a3
YeetingWaterbottle <73762047+YeetingWaterbottle@users.noreply.github.com>
2023-03-16 16:59:01 -0700

    making the bookmarklet actually work correctly

--- UTexas-Chemistry/enable-answer-box-scrolling.js
+++ UTexas-Chemistry/enable-answer-box-scrolling.js
@@ -1,9 +1,31 @@
 // Bookmarklet Created With Bookmarkleter: https://chriszarate.github.io/bookmarkleter/
 // Copy the line below and create a bookmark with the url field set to it ⤵
-javascript:void function(){document.querySelector(".dataTables_scrollBody").style="position: unset !important; overflow: unset !important; height: unset !important;",document.querySelector(".dataTables_scrollBody > .table").style="position: unset !important;",document.querySelector(".dataTables_scrollBody > div").style="height: inherit !important;",document.querySelector(".col-sm-12").style="max-height: 1200px !important; overflow: auto !important;"}();
+javascript:void function(){let a=document.createElement("style");a.innerText=".dataTables_scrollBody {position: unset !important;overflow: unset !important;height: unset !important;}.dataTables_scrollBody > .table {position: unset !important;}.dataTables_scrollBody > div {height: inherit !important;}.col-sm-12 {max-height: 1200px !important;overflow: auto !important;}",document.head.appendChild(a)}();
+
 /* Original Javascript Code Below
-document.querySelector('.dataTables_scrollBody').style = 'position: unset !important; overflow: unset !important; height: unset !important;';
-document.querySelector('.dataTables_scrollBody > .table').style = 'position: unset !important;';
-document.querySelector('.dataTables_scrollBody > div').style = 'height: inherit !important;';
-document.querySelector('.col-sm-12').style = 'max-height: 1200px !important; overflow: auto !important;';
+let custom_style = `
+    .dataTables_scrollBody {
+        position: unset !important;
+        overflow: unset !important;
+        height: unset !important;
+    }
+    
+    .dataTables_scrollBody > .table {
+        position: unset !important;
+    }
+    
+    .dataTables_scrollBody > div {
+        height: inherit !important;
+    }
+
+    .col-sm-12 {
+        max-height: 1200px !important;
+        overflow: auto !important;
+    }
+`
+
+let custom_style_element = document.createElement("style")
+custom_style_element.innerText = custom_style
+document.head.appendChild(custom_style_element)
+
 */
